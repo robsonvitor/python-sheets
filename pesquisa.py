4dd80d4c7dd1971bc1fc89f251caedb88a39d6d6
Robson Vitor Mendonça <robsonvitorm@gmail.com>
2021-10-09 07:13:14 -0300

    creds e telegram_send tutorial

--- pesquisa.py
+++ pesquisa.py
@@ -12,6 +12,7 @@ scope = ["https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive.file",
         "https://www.googleapis.com/auth/drive"]
 
+# https://docs.gspread.org/en/v4.0.1/oauth2.html
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
 client = gspread.authorize(creds)
@@ -25,5 +26,6 @@ criteria_re = re.compile(r'(STRING1|STRING2)', re.IGNORECASE)
 rs = ws.findall(criteria_re)
 
 if rs:
-    telegram_send.send(messages=["Diploma pronto!!!!"])
+    # https://medium.com/@robertbracco1/how-to-write-a-telegram-bot-to-send-messages-with-python-bcdf45d0a580
+    telegram_send.send(messages=["Encontrado!!!!"])
 
